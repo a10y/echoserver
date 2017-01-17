8251b74e4af54fe93aeb74f655b8a1cd8af7e971
Andrew Duffy <root@aduffy.io>
2017-01-16 16:05:24 -0800

    refactor a little

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,12 +2,21 @@ module Lib (
     echoListen
 ) where
 
+import qualified Data.ByteString.Lazy as LB
+
 import Network as N
 import Network.Socket as NS
 import Network.Socket.ByteString.Lazy as NSB
 
-import qualified Data.ByteString.Lazy as LB
 import Control.Concurrent
+
+-- | Listens on the socket forever, sending back all data received.
+-- Loops until the client hangs up.
+echoTCP :: Socket -> IO ()
+echoTCP client = do c <- NSB.recv client 1024
+                    if not (LB.null c)  -- Stop serving echoes when client hangs up
+                      then NSB.sendAll client c >> echoTCP client
+                      else return ()
 
 -- | Listen on the provided port, echoing responses back to clients.
 -- Listen indefinitely, spawning off a new green thread to serve each client.
@@ -17,16 +26,10 @@ echoListen port = do sock <- listenOn $ PortNumber (fromIntegral port :: PortNum
                      putStrLn $ "Listening on port " ++ show chosenPort
                      runServer sock
                   where
-                    runServer s = do
-                      (client, addr) <- NS.accept s
-                      print $ "new client from " ++ (show addr)
-                      -- Fork off to serve new client
-                      forkIO $ do
-                        echo client
-                        close client
-                      runServer s
-                    echo client = do
-                      c <- NSB.recv client 1024
-                      if not (LB.null c)  -- Stop serving echoes when client hangs up
-                        then NSB.sendAll client c >> echo client
-                        else return ()
+                    runServer s = do (client, addr) <- NS.accept s
+                                     print $ "new client from " ++ (show addr)
+                                     -- Fork off to serve new client
+                                     forkIO $ do
+                                        echoTCP client
+                                        close client
+                                     runServer s
